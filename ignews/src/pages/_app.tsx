21cf1cb261fe69e160e4f2d8acbae842e3c8f0b8
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-03-29 14:21:21 -0300

    header

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
-import { Head } from "next/document";
+import Header from "../components/Header";
+
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <Header />
       <Component {...pageProps} />
+    </>
   );
 }
 
