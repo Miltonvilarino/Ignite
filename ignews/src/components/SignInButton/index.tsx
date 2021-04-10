90a45039efb034ab8187fc8b11e6d4405180244c
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-04-10 17:29:31 -0300

    nextAuth implement

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,21 +1,32 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 const SignInButton: React.FC = () => {
-    const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="submit" className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      type="submit"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Milton Vilarino
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="submit" className={styles.signInButton}>
-    <FaGithub color="#eba417" />
-    Sign in with GitHub
-  </button>
+    <button
+      type="submit"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
   );
 };
 
