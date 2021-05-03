f691cc80baa759942ebab74f26cc50b6274fffad
Milton Vilarino Netto <miltonvilarino@gmail.com>
2021-05-03 06:23:28 -0300

    integrando signin com react hook form

--- dashgo/src/components/Form/Input.tsx
+++ dashgo/src/components/Form/Input.tsx
@@ -1,35 +1,43 @@
 import {
-  Input as ChakraInput,
-  InputProps as ChakraInputProps,
-  FormLabel,
   FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as CkakraInput,
+  InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-const Input = ({ name, label, ...rest }: InputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest }: InputProps,
+  ref
+) => {
   return (
-    <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label} </FormLabel>}
-
-      <ChakraInput
-        name="password"
-        type="password"
-        id="password"
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <CkakraInput
+        name={name}
+        id={name}
         focusBorderColor="pink.500"
-        bgColor="gray.900"
+        backgroundColor="gray.900"
         variant="filled"
         _hover={{
-          bgColor: "gray.900",
+          backgroundColor: "gray.900",
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
 };
 
-export default Input;
+export const Input = forwardRef(InputBase);
--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,24 +1,73 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
-import Input from "../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export default function Home() {
+import { Input } from "../components/Form/Input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("e-mail obrigatório").email("e-mail inválido"),
+  password: yup.string().required("senha obrigatória"),
+});
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
-    <Flex w="100vw" h="100vh" align="center" justify="center">
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Flex
         as="form"
-        w="100%"
+        width="100%"
         maxWidth={360}
-        bg="gray.800"
-        p="8"
+        background="gray.800"
+        padding="8"
         borderRadius={8}
-        flexDir="column"
+        flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            error={errors.password}
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
